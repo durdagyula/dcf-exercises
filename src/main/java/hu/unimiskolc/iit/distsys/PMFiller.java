2baa788e3b8d77e0d79d5daa3be1990ce393006c
durda <gyuladurda@gmail.com>
2015-10-06 08:08:15 +0200

    test 2

--- src/main/java/hu/unimiskolc/iit/distsys/PMFiller.java
+++ src/main/java/hu/unimiskolc/iit/distsys/PMFiller.java
@@ -2,27 +2,60 @@ package hu.unimiskolc.iit.distsys;
 
 import hu.mta.sztaki.lpds.cloud.simulator.Timed;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.IaaSService;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.VMManager.VMManagementException;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.VirtualMachine;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.AlterableResourceConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ConstantConstraints;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ResourceConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.io.NetworkNode.NetworkException;
 import hu.mta.sztaki.lpds.cloud.simulator.io.VirtualAppliance;
 import hu.unimiskolc.iit.distsys.interfaces.FillInAllPMs;
 
-public class PMFiller implements FillInAllPMs {
+
+public class PMFiller implements FillInAllPMs{
+
+	@Override
+	public void filler(IaaSService iaas, int vmCount) {
+		// TODO Auto-generated method stub
 	
-		public void filler(IaaSService iaas, int vmCount) {
+		
+		VirtualAppliance va = new VirtualAppliance("Virtaulappliance", 1, 0);
+		VirtualMachine vm = new VirtualMachine(va);
+		ResourceConstraints rc = new AlterableResourceConstraints(20.0, 2000.0, 4096);
+		
+		
+		Timed.simulateUntilLastEvent();	
+		
+		for (PhysicalMachine iaas2 : iaas.machines){
+			try {
+
+				iaas.registerRepository(iaas2.localDisk);
+				iaas2.localDisk.registerObject(va);
 			
-			VirtualAppliance va = new VirtualAppliance("va",1,0);
-			
-			for (int i=0;i<10;i++)
-			{
-				try{ 
-					ResourceConstraints asd = iaas.machines.get(i).getCapacities();
-					ResourceConstraints rcc = new AlterableResourceConstraints(asd.getRequiredCPUs(),asd.getRequiredProcessingPower(),asd.getRequiredMemory());
-					iaas.requestVM(va,rcc,iaas.machines.get(i).localDisk,iaas.machines.size());
-				}
-				catch (Exception e){}
+			//	System.out.println(iaas.machines.get(i).getCapacities());
+				double c = iaas2.getCapacities().getRequiredCPUs();
+				double p = iaas2.getCapacities().getRequiredProcessingPower();
+				long m = iaas2.getCapacities().getRequiredMemory();
+				
+				rc = new ConstantConstraints(c/10, p/10, m/10);
+				
+				vm = iaas.requestVM(va, rc, iaas2.localDisk, iaas.machines.size())[0];
+				Timed.simulateUntilLastEvent();
+				
+				System.out.println(vm.getState());
+			//	System.out.println(vm.getTotalProcessed());
+				System.out.println(iaas.listVMs().size());
+			//	Timed.simulateUntilLastEvent();
+				
+			} catch (VMManagementException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NetworkException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-			Timed.simulateUntilLastEvent();
 		}
+		
+	}
 }
